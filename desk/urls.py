093575269001ccff228e3e56b762ba5de1a4610c
Anton Sharapov <sharapovproject@yandex.ru>
2020-03-11 20:49:04 +0700

    rr

--- desk/urls.py
+++ desk/urls.py
@@ -18,7 +18,7 @@ from . import views
 from django.urls import include, path
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.desk_index, name='index'),
 ]
 
 
--- desk/views.py
+++ desk/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+def desk_index(request):
+    print()
+    print(request)
+    print()
+    for d in dir(request):
+        print(d)
+    print()
+
     return HttpResponse("<h1>Hello, world. You're at the polls index.</h1>")
